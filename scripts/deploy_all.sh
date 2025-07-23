8ec5f717b85a381d4431053449f51000a868c8d9
spacesthree <spacesthree@proton.me>
2025-07-23 18:19:32 +0000

    updated

--- scripts/deploy_all.sh
+++ scripts/deploy_all.sh
@@ -18,6 +18,11 @@ helm install ingress-nginx ingress-nginx/ingress-nginx \
   --namespace ingress-nginx
 ./apply_k8s.sh
 
+kubectl create ns monitoring
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo update
+helm install prometheus-stack prometheus-community/kube-prometheus-stack \
+  --namespace monitoring
 kubectl patch svc prometheus-stack-grafana \
   -n monitoring \
   -p '{"spec": {"type": "LoadBalancer"}}'
